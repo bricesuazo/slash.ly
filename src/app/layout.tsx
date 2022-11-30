282d728f9c9a5e4e19d3685a69768f70ee99735f
Brice Suazo <bricesuazo@gmail.com>
2022-11-30 18:04:40 +0800

    Add Poppins font from @next/font.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,18 @@
-import './globals.css'
+import "./globals.css";
+import { Poppins } from "@next/font/google";
 
+const poppins = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html lang="en" className={poppins.className}>
       <head />
       <body>{children}</body>
     </html>
-  )
+  );
 }
