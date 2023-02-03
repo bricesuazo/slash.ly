47a09ad4fc433a4f7ce7263d6b9d581ebd462f49
Brice Suazo <bricesuazo@gmail.com>
2023-02-03 19:08:43 +0800

    Migrate to native nextjs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,28 +1,12 @@
-import "../styles/globals.css";
-import { type AppType } from "next/app";
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
-import { Poppins } from "@next/font/google";
 
-import { trpc } from "../utils/trpc";
-
-const poppins = Poppins({
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-});
-
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
+export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={poppins.className}>
-      <SessionProvider session={session}>
-        <Component {...pageProps} />
-        <Analytics />
-      </SessionProvider>
-    </main>
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
   );
-};
-
-export default trpc.withTRPC(MyApp);
+}
--- src/pages/api/link/[shortUrl].ts
+++ src/pages/api/link/[shortUrl].ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-import { prisma } from "../../../server/db/client";
+import { prisma } from "../../../utils/prisma";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const shortUrl = req.query["shortUrl"];
